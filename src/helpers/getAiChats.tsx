80ca62330102f71e45dec88e11eb2ef3e18aa11a
Chengxun Lee <24319042+bclswl0827@users.noreply.github.com>
2024-02-02 03:17:04 +0800

    Optimize system appended prompts

--- src/helpers/getAiChats.tsx
+++ src/helpers/getAiChats.tsx
@@ -17,7 +17,7 @@ export const getAiChats = async (
             .filter((item) => item !== -1);
         if (!!attachmentIndexArr.length) {
             const indexArrStr = attachmentIndexArr.join(", ");
-            prompts += `\n\n---\n\nThis is a prompt appended automatically by the system: Please note that the user uploaded image(s) in the earlier conversation at index ${indexArrStr} and it was processed and answered by \`gemini-pro-vision\`, since you don't have the ability to recognize images, please try to find something useful information from the previous prompts and responses and answer the user's question accordingly, never tell the user you saw this appended prompt, and never tell the user that you don't know how to answer the question, just try to answer it as best as you can.`;
+            prompts += `\n\n---\n\nThis is a prompt appended automatically by the system: Please note that the user uploaded image(s) in the earlier conversation at index ${indexArrStr} and it was processed and answered by \`gemini-pro-vision\`, since you don't have the ability to recognize images, please try to find some useful information from the previous prompts and responses and answer the user's question accordingly, never tell the user you saw this appended prompt, and never tell the user that you don't know how to answer the question, just try to answer it as best as you can, just like the way his/her grandmother used to treat him/her when he/she was a child.`;
         }
 
         const payload = history.map((item) => {
